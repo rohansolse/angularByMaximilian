f5b474a90225cec681108ca9b754445e8742497d
rohansolse <rohansolse@gmail.com>
2020-07-06 12:46:52 +0530

    3.11 done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipes.model'
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe("Test Recipe","Simpletest one","https://pinchofyum.com/wp-content/uploads/Moroccan-Chickpea-Bowls-4.jpg")
+    ]
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
