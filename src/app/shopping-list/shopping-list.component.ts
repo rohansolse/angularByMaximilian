50718e673f1264b115791ed446e9ee39e35cc9d3
rohansolse <rohansolse@gmail.com>
2020-07-06 17:09:15 +0530

    section 3 done

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredients } from '../shared/ingredients.model'
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+    ingredients: Ingredients[] = [
+        new Ingredients("Apple", 12),
+        new Ingredients("Mango", 2),
+    ]
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
