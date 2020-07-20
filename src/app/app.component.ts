d34d89e3aecf45a87b6a433d0998484c4b026e72
rohansolse <rohansolse@gmail.com>
2020-07-20 11:23:51 +0530

    6.5 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    serverElements = [{
+        name : "Test server",
+        type : "server",
+        content : "Just a Content!"
+    }];
+
+    onServerAdded(severData:{serverName:'string',serverContent:'string'}) {
+        this.serverElements.push({
+            type: 'server',
+            name: severData.serverName,
+            content: severData.serverContent
+        });
+    }
+
+    onBlueprintAdded(blurprintData:{serverName:'string',serverContent:'string'}) {
+        this.serverElements.push({
+            type: 'blueprint',
+            name: blurprintData.serverName,
+            content: blurprintData.serverContent
+        });
+    }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CockpitComponent,
+        ServerElementComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
