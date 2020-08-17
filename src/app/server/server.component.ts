4de9cb72e88f1ff82541ed3b237e9fcd07eb6bf4
rohansolse <rohansolse@gmail.com>
2020-08-17 18:38:22 +0530

    create branch for first lesson

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-server',
-    templateUrl:'./server.component.html',
+    selector: 'app-server',
+    templateUrl: './server.component.html',
     styleUrls: ['./server.component.css']
 })
 
-export class ServerComponent{
+export class ServerComponent {
     serverId: number = 10
     serverStatus: string = "online"
-    constructor(){
+    constructor() {
         this.serverStatus = Math.random() > 0.5 ? "online" : "offline"
         this.serverId = Math.random() > 0.5 ? 10 : 8
     }
-    getServerName(){
+    getServerName() {
         return Math.random() > 0.5 ? "Production Server" : "Dev Server"
     }
-    getColor(){
+    getColor() {
         return this.serverStatus == "online" ? "green" : "red"
     }
 }
