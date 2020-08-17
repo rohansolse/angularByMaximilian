313837b9d333438cd9fd6f115c077eb33eb6f71c
rohansolse <rohansolse@gmail.com>
2020-08-17 16:35:52 +0530

    6.4 done

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipes.model';
 
 @Component({
@@ -8,9 +8,14 @@ import { Recipe } from '../../recipes.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe
+  @Output() receivedRecipeItem = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectRecipe() {
+    this.receivedRecipeItem.emit()
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipes.model'
 @Component({
     selector: 'app-recipe-list',
@@ -6,14 +6,19 @@ import { Recipe } from '../recipes.model'
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    @Output() recivedRecipeList = new EventEmitter<Recipe>();
     recipes: Recipe[] = [
-        new Recipe("Test Recipe", "Simpletest one", "https://pinchofyum.com/wp-content/uploads/Moroccan-Chickpea-Bowls-4.jpg"),
-        new Recipe("Test Recipe", "Simpletest one", "https://pinchofyum.com/wp-content/uploads/Moroccan-Chickpea-Bowls-4.jpg")
+        new Recipe("Test Recipe 01", "Simpletest 01 one", "https://pinchofyum.com/wp-content/uploads/Moroccan-Chickpea-Bowls-4.jpg"),
+        new Recipe("Test Recipe 02", "Simpletest  02 one", "https://pinchofyum.com/wp-content/uploads/Moroccan-Chickpea-Bowls-4.jpg")
 
     ]
     constructor() { }
 
     ngOnInit() {
+    }
+
+    selectRecipe(recipe: Recipe) {
+        this.recivedRecipeList.emit(recipe)
     }
 
 }
