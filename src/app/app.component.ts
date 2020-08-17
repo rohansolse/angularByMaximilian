71a4c99ad9812545febb6365ac9699ed2226c39d
rohansolse <rohansolse@gmail.com>
2020-08-17 14:18:07 +0530

    5.16 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     serverElements = [{
-        name : "Test server",
-        type : "server",
-        content : "Just a Content!"
+        name: "Test server",
+        type: "server",
+        content: "Just a Content!"
     }];
 
-    onServerAdded(severData:{serverName:'string',serverContent:'string'}) {
+    onServerAdded(severData: { serverName: 'string', serverContent: 'string' }) {
         this.serverElements.push({
             type: 'server',
             name: severData.serverName,
@@ -20,12 +20,20 @@ export class AppComponent {
         });
     }
 
-    onBlueprintAdded(blurprintData:{serverName:'string',serverContent:'string'}) {
+    onBlueprintAdded(blurprintData: { serverName: 'string', serverContent: 'string' }) {
         this.serverElements.push({
             type: 'blueprint',
             name: blurprintData.serverName,
             content: blurprintData.serverContent
         });
+    }
+
+    onChangeFirst() {
+        this.serverElements[0].name = "changed!"
+    }
+
+    onDistroyCalled() {
+        this.serverElements.splice(0, 1)
     }
 
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
     selector: 'app-server-element',
@@ -6,11 +6,46 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
     styleUrls: ['./server-element.component.css'],
     // encapsulation:ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit {
-    @Input('srvElement') element: {name: string, type: string, content: string}
-    constructor() { }
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck,
+    AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+    @Input('srvElement') element: { name: string, type: string, content: string }
+    @Input() name: string;
+    constructor() {
+        console.log("constructor called!");
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        console.log("ngOnChanges called!");
+        console.log(changes);
+    }
 
     ngOnInit(): void {
+        console.log("ngOnInit called!");
     }
+
+    ngDoCheck() {
+        console.log("ngDoCheck");
+    }
+
+    ngAfterContentInit() {
+        console.log("ngAfterContentInit");
+    }
+
+    ngAfterContentChecked() {
+        console.log("ngAfterContentChecked");
+    }
+
+    ngAfterViewInit() {
+        console.log("ngAfterViewInit");
+    }
+
+    ngAfterViewChecked() {
+        console.log("ngAfterViewChecked");
+    }
+
+    ngOnDestroy() {
+        console.log("ngOnDestroy");
+    }
+
 
 }
