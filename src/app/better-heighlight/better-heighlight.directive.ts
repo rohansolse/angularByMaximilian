2e34c753c95b5608d7e978093fe22e6b9eeb81ae
rohansolse <rohansolse@gmail.com>
2020-08-18 14:48:50 +0530

    7.5 done

--- src/app/better-heighlight/better-heighlight.directive.ts
+++ src/app/better-heighlight/better-heighlight.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHeighlight]'
 })
-export class BetterHeighlightDirective {
+export class BetterHeighlightDirective implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
+
+  ngOnInit() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue')
+  }
 
 }
