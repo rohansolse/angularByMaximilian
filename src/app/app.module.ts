a5887b7ddfeab0e0f33963605f7deaa3a28acaba
rohansolse <rohansolse@gmail.com>
2020-08-26 00:50:26 +0530

    9.2 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
 @NgModule({
   declarations: [
     AppComponent,
+    AccountComponent,
+    NewAccountComponent,
   ],
   imports: [
     BrowserModule,
